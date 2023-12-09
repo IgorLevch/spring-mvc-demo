ad53aa194f82bf9fb704ef2693735f128d7d7921
IgorLevch <igorlevvv@gmail.com>
2023-12-09 17:22:30 +0300

    Lesson 3

---
+++ src/main/java/ru/gb/api/product/ProductController.java
@@ -0,0 +1,59 @@
+package ru.gb.api.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+
+
+    private ProductRepository prRep;
+
+    @Autowired
+    public ProductController(ProductRepository prRep) {
+        this.prRep = prRep;
+    }
+
+    @GetMapping("/all")
+    public String getAllProducts(Model model){
+        List<Product> products = prRep.getAll();
+        model.addAttribute("products", products);
+        return "products2";
+
+    }
+    @GetMapping("/{id}")
+    public String editPage(@PathVariable("id")Long id,Model model){
+        // Конструкция из @PathVariable и шаблона {id} заставит Спринг МВС извлечь элемент {id} из УРЛ
+        // и преобразовать его в тип ЛОнг и передать в виде параметра этого метода
+        model.addAttribute("product",prRep.getById(id));
+        return "products3";
+    }
+   /* @GetMapping("/{id}")
+    public String editPage(@PathVariable("id")Long id,Model model){
+        // Конструкция из @PathVariable и шаблона {id} заставит Спринг МВС извлечь элемент {id} из УРЛ
+        // и преобразовать его в тип ЛОнг и передать в виде параметра этого метода
+        List<Product> products = (List<Product>) prRep.getById(Id);
+        model.addAttribute("product",products);
+        return "products3";
+    }*/
+
+
+
+
+
+    @PostMapping("/update")
+    public String update(Product product){
+    prRep.update(product);
+    return "redirect:/product";
+    }
+
+
+
+
+}
---
+++ src/main/java/ru/gb/api/product/ProductRepository.java
@@ -0,0 +1,41 @@
+package ru.gb.api.product;
+
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+
+@Repository
+public class ProductRepository {
+
+    private List<Product> products;
+
+    // @PostConstruct   эта аннотация означает, что сразу, после того, как данный бин создан и инициализирован,
+    // будет вызван данный метод (перед выполнением каких-либо еще действий).
+
+
+    public List<Product> getAll(){
+        products =List.of(
+             new Product(),
+             new Product(),
+             new Product(),
+             new Product());
+
+        return products;
+    }
+
+    public Product getById(Long Id){
+        return products.get(Math.toIntExact(Id));
+        //return products.stream().filter(p->p.getId().equals(Id)).findFirst().orElseThrow(()->new RuntimeException());
+    }
+
+
+
+
+    public void update(Product product) {
+        products.add(product);
+    }
+
+
+}
