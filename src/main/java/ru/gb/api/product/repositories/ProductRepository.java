8e3046ec3218b7b39dfc7130da9b1c6bc12d74c7
IgorLevch <igorlevvv@gmail.com>
2023-12-26 06:13:56 +0300

    Lesson 4. Usharovsky. Spring Boot

--- src/main/java/ru/gb/api/product/repositories/ProductRepository.java
+++ src/main/java/ru/gb/api/product/repositories/ProductRepository.java
@@ -3,6 +3,7 @@ package ru.gb.api.product.repositories;
 import org.springframework.stereotype.Repository;
 import ru.gb.api.product.data.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -34,10 +35,22 @@ public class ProductRepository {
                 new Product(78L,"jjhh",876L,80),
                 new Product(9876L,"hgfre",98L,80));
 
-
+        // List.of - это метод, который служит для создания иммутабельного Листа
         return products;
 
     }
+    //Можно попробовать записать вот так (чтобы уйти от иммутабельности при записи в виде: List.of():
+   /* public List<Product> getAll(){
+        products =new ArrayList<>(List.of(
+                new Product(1L,"ggh",45L,80),
+                new Product(2L,"hhj",78L,80),
+                new Product(78L,"jjhh",876L,80),
+                new Product(9876L,"hgfre",98L,80)));
+
+        // List.of - это метод, который служит для создания иммутабельного Листа
+        return products;
+
+    }*/
 
 
 
