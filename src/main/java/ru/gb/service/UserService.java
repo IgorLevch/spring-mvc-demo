7030275652257558379e1d8cd36390b9d1d78561
IgorLevch <igorlevvv@gmail.com>
2024-02-11 19:24:54 +0300

    Lesson 7. Usharovsky. Spring Data JPA

--- src/main/java/ru/gb/service/UserService.java
+++ src/main/java/ru/gb/service/UserService.java
@@ -4,16 +4,20 @@ package ru.gb.service;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
      List<UserRepr> findAll();
 
-     UserRepr findById(long id);
+    // после добавления наследования от JPARepository добавляем Optional
+     Optional<UserRepr> findById(long id);
 
-    void insert(UserRepr user);
+    void save(UserRepr user);
 
-    void update(UserRepr user);
+  //  void update(UserRepr user);  после добавления JPARep -- нам этот метод не нужен
 
     void delete(long id);
+
+    List<UserRepr> findWithFilter(String usernameFilter);
 
 }
